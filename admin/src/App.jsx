361862b6f04804954464cbabe7d618fd14cafae5
Mhamzashamsi <hamzashamsi270@gmail.com>
2026-09-12 18:42:25 +0500

    admin app

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,3 +1,42 @@
+// import React from 'react'
+// import { Route, Routes } from 'react-router-dom'
+// import Home from './pages/Home'
+// import List from './pages/List'
+// import Add from './pages/Add'
+// import Orders from './pages/Orders'
+// import Login from './pages/Login'
+// import { Admindatacontext } from './Context/Admincontext'
+// import { useContext } from 'react'
+//   import { ToastContainer, toast } from 'react-toastify';
+
+
+// function App() {
+//   let admindata = useContext(Admindatacontext)
+//   return (
+//     <>
+//  <ToastContainer />
+// { !admindata ? <Login /> : <>
+
+// <Routes>
+// <Route path='/' element={<Home />} />
+// <Route path='/list' element={<List />} />
+// <Route path='/add' element={<Add />} />
+// <Route path='/orders' element={<Orders />} />
+// <Route path='/login' element={<Login />} />
+
+// </Routes>
+//     </>
+
+// }
+
+
+
+
+//     </>
+//   )
+// }
+
+// export default App
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -8,6 +47,7 @@ import Login from './pages/Login'
 import { Admindatacontext } from './Context/Admincontext'
 import { useContext } from 'react'
   import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
